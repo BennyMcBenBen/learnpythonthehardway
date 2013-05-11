8403917745d6fbf1b3b089b2f1c2c6e7deae2977
Benjamin Zoller <benjamin.zoller@gmail.com>
2013-05-11 15:40:50 -0400

    ex16 study drill

--- ex16.py
+++ ex16.py
@@ -12,7 +12,7 @@ print("Opening the file...")
 target = open(filename, 'w')
 
 print("Truncating the file. Goodbye!")
-target.truncate()
+target.truncate() # Unnecessary
 
 print("Now I'm going to ask you three lines.")
 
@@ -22,12 +22,7 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write("{}\n{}\n{}\n".format(line1, line2, line3))
 
 print("And finally, we close it.")
 target.close()
