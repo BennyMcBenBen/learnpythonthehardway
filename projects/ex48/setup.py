4f7d88b7c4aa547a08288028be2dceca78eb237d
Benjamin Zoller <benjamin.zoller@gmail.com>
2013-05-30 18:02:32 -0400

    ex48

--- projects/ex48/setup.py
+++ projects/ex48/setup.py
@@ -4,16 +4,16 @@ except ImportError:
     from distutils.core import setup
     
 config = {
-    'description': 'My Project',
+    'description': 'ex48',
     'author': 'Ben Zoller',
     'url': 'URL to get it at.',
     'download_url': 'Where to download it.',
     'author_email': 'My email.',
     'version': '0.1',
     'install_requires': ['nose'],
-    'packages': ['NAME'],
+    'packages': ['ex48'],
     'scripts': [],
-    'name': 'projectname'
+    'name': 'ex48'
 }
 
 setup(**config)
